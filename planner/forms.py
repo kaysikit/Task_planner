b2d524f71d5c002028d4c8de7981dac38b854b5c
kaysikit <kaysik_it@mail.ru>
2023-08-28 11:38:46 +0500

    Revision of the form

--- planner/forms.py
+++ planner/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Task
+from datetime import date
 
 
 class AddTaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['end_date'].initial = date.today()
+
     class Meta:
         model = Task
         fields = ['title', 'task', 'status',  'end_date']
@@ -10,6 +15,6 @@ class AddTaskForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control mb-3'}),
             'task': forms.Textarea(attrs={'class': 'form-control mb-3'}),
-            'end_date': forms.SelectDateWidget(attrs={'class': 'form-select mb-3 '}),
+            'end_date': forms.SelectDateWidget(attrs={'class': 'form-control mb-3'}),
             'status': forms.Select(attrs={'class': 'form-select mb-3'})
         }
